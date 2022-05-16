915185ad2d63d25f848f00587c043b93fe21b55f
Haruya-Yajima <haruya.yajima@toyota.global>
2022-05-16 18:54:09 +0900

    feat:getURL & send to slack
    Co-authored-by: KazunoriNakajima <kazunorinakajima@users.noreply.github.com>
    Co-authored-by: Haruya-Yajima <haruya-yajima@users.noreply.github.com>

--- popup.js
+++ popup.js
@@ -1,27 +1,11 @@
-// document.getElementById("btn").addEventListener("click", async () => {
-//   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-//   chrome.scripting.executeScript({
-//     target: { tabId: tab.id },
-//     function: onRun,
-//   });
-// });
-
-// function onRun() {
-//   document.body.style.backgroundColor = "#fcc";
-// }
-
-// window.addEventListener("load", async () => {
 document.getElementById("btn").addEventListener("click", async () => {
-  let url = "";
-  // async function getCurrentTab() {
-  let queryOptions = { active: true, currentWindow: true };
-  let [tab] = await chrome.tabs.query(queryOptions);
-  //   return tab;
-  // }
-  // chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
-  //   url = tabs[0].url;
-  //   console.log(tabs);
-  // });
-  const URLEl = document.getElementsByClassName("URL");
-  URLEl[0].innerText = "URL: " + typeof tab;
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: onRun,
+  });
 });
+
+function onRun() {
+  document.body.style.backgroundColor = "#fcc";
+}
