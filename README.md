a79960000c7d9007a457d4d72b846b7e5263334c
KazunoriNakajima <kazunori_nakajima_aa@mail.toyota.co.jp>
2022-05-17 00:26:17 +0900

    feat:README

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # ChromeExtention-for-posting-interestings
+
+## コンセプト
+
+この課題を進めるにあたって、私たち二人で考えたコンセプトは以下の２点です。
+
+1. 使いやすく、操作が簡単なもの
+2. 頻度の高い作業でみんな使ってもらえるもの
+
+   →Chrome 拡張機能で interests へ瞬間投稿！気になるニュースはすぐに共有！！
+
+## 設定＆使い方
+
+設定
+
+1. Chrome 拡張機能の設定　 → Chrome ウェブストアからダウンロード
+2. Slack API の設定　 → Incoming Webhook
+
+使い方
+
+1. Chrome 拡張機能の設定
+2. Slack API の設定
--- test/_popup.js
+++ test/_popup.js
@@ -2,15 +2,7 @@ const popup = require("../popup.js");
 const { expect } = require("chai");
 
 describe("popup", () => {
-  it("should find total population", (done) => {
-    // Setup
-    const expected = 561453504;
-    // Exercise
-    populatron.totalPopulation((actual) => {
-      // Assert
-      expect(actual).to.equal(expected);
-      // Teardown
-      done();
-    });
+  it("should return ", () => {
+    expect(sendMessage).to.be.a("function");
   });
 });
