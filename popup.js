cd9ba27997ba0b232d171ddf9e41fc576d5f4dbe
Haruya-Yajima <haruya.yajima@toyota.global>
2022-05-17 12:35:20 +0900

    feat:add hidden func

--- popup.js
+++ popup.js
@@ -5,6 +5,14 @@ window.addEventListener("load", async () => {
   let queryOptions = { active: true, currentWindow: true };
   let [tab] = await chrome.tabs.query(queryOptions);
 
+  let yourWebhookURL = localStorage.getItem("yourWebhookURL");
+  if (!yourWebhookURL) {
+    // Prompt for one if a username isn't found
+    yourWebhookURL = window.prompt("slackのyourWebhookURLは？");
+    localStorage.setItem("yourWebhookURL", yourWebhookURL);
+  }
+
+
   const URLEl = document.getElementsByClassName("URL");
   URLEl[0].innerText = "URL: " + tab.url;
   document.getElementById("btn-post").addEventListener("click", () => {
@@ -16,7 +24,9 @@ window.addEventListener("load", async () => {
         "明日のinterestsで紹介したい",
       ];
       const webhookURL =
-        "https://hooks.slack.com/services/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";
+      yourWebhookURL;
+      // "https://hooks.slack.com/services/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";
+        // "https://hooks.slack.com/services/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";
       // "https://hooks.slack.com/services/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";
       // "https://hooks.slack.com/services/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";
       for (let i = 1; i <= 3; i++) {
@@ -51,8 +61,18 @@ window.addEventListener("load", async () => {
     sendMessage();
     textForm.value = "";
   });
+  document.getElementById("btn-setting").addEventListener("click", () => {
+    yourWebhookURL = window.prompt("slackのyourWebhookURLを入力");
+    localStorage.setItem("yourWebhookURL", yourWebhookURL);
+  });
+  const impersonBox = document.getElementById("impersonation-box");
+  impersonBox.style.display = "none";
+  const impersonationButtonElement = document.getElementById("btn-impersonation");
+  impersonationButtonElement.addEventListener("click", ()=>{
+    if (impersonBox.style.display === "block") {
+      impersonBox.style.display = "none";
+    } else {
+      impersonBox.style.display = "block";
+    }
+  });
 });
-
-function onRun() {
-  document.body.style.backgroundColor = "#fcc";
-}
