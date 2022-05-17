683e894663b0fe8a4b60e334f98aff31e1e85bd9
KazunoriNakajima <kazunori_nakajima_aa@mail.toyota.co.jp>
2022-05-17 09:40:33 +0900

    feat:webhook URL changed

--- popup.js
+++ popup.js
@@ -16,7 +16,7 @@ window.addEventListener("load", async () => {
         "明日のinterestsで紹介したい",
       ];
       const webhookURL =
-        "https://hooks.slack.com/services/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";
+        "https://hooks.slack.com/services/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";
       // "https://hooks.slack.com/services/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";
       // "https://hooks.slack.com/services/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";
       for (let i = 1; i <= 3; i++) {
@@ -36,7 +36,7 @@ window.addEventListener("load", async () => {
         text: `<${tab.url}>\n${message}`,
 
         username: "masashi",
-        icon_url: "image/T02DS9VKWLU-U02RZU1281G-227adde3fd71-512.png",
+        // icon_url: "image/T02DS9VKWLU-U02RZU1281G-227adde3fd71-512.png",
       };
       fetch(webhookURL, {
         method: "POST",
